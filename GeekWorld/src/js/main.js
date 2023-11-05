2ead8adb4e4436ee08fdc02e0de97c747c7d2393
Flavio05Henrique <117491467+Flavio05Henrique@users.noreply.github.com>
2023-11-05 16:55:09 -0300

    Update seção 2 iniciada.
    -add cards

--- GeekWorld/src/js/main.js
+++ GeekWorld/src/js/main.js
@@ -4,12 +4,21 @@ const windowHeight = window.innerHeight;
 console.log("H: " + windowHeight + " W: " + windowWidth);
 const imgsSec1 = document.querySelectorAll('[data="secHome__imgStatic"]');
 const homeComponents = document.querySelectorAll('[data="secHome"]');
-console.log(imgsSec1);
+const sec2 = document.querySelector('[data="secTopA"]');
+const sec2ComponentsCards = sec2.querySelectorAll('[data="card"]');
+console.log(sec2ComponentsCards);
 document.addEventListener('click', event => {
-    imgsSec1.forEach(img => {
-        img.classList.toggle('translateZero');
-    });
-    homeComponents.forEach(comp => {
-        comp.classList.toggle('opacityOff');
+    // imgsSec1.forEach(img => {
+    //     img.classList.toggle('translateZero')
+    // })
+    // homeComponents.forEach(comp => {
+    //     comp.classList.toggle('opacityOff')
+    // })
+    let count = 0;
+    sec2ComponentsCards.forEach(e => {
+        setTimeout(() => {
+            e.classList.toggle('translateZero');
+        }, 100 * count);
+        count++;
     });
 });
