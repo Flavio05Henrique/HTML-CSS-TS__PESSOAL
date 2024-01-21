86d36a94932b8dae342e97adddf8975f90380b66
Flavio05Henrique <117491467+Flavio05Henrique@users.noreply.github.com>
2024-01-21 19:15:39 -0300

    .

--- MinhaGaleriaDeObras/src/js/main.js
+++ MinhaGaleriaDeObras/src/js/main.js
@@ -6,6 +6,25 @@ const createNewCardBnt = document.querySelector('[data="createNewCardBnt"]');
 const cardExtendedCreateNewCard = document.querySelector('[data="cardExtended__CreateNewCard"]');
 const activeCardBnt = document.querySelector('[data="activeCardBnt"]');
 const cardsContainer = document.querySelector('[data="cards__container"]');
+const popUpContainerDynamic = document.querySelector('[data="popUpContainerDynamic"]');
+const popUpContainerDynamicCloseBnt = document.querySelector('[data="popUpContainerDynamic__bntClose"]');
+const popUpContainerDynamicContainer = document.querySelector('[data="popUpContainerDynamicConatiner"]');
+let MyList = [];
+let CurrentExtendedCard;
+const loadMyList = () => {
+    const list = localStorage.getItem('MinhaListaDeObras');
+    if (list == null)
+        return;
+    MyList = JSON.parse(list);
+    console.log(MyList);
+};
+const loadMyListInDom = () => {
+    const cards = [];
+    MyList.forEach(card => {
+        cards.push(makeCardInDom(card));
+    });
+    cardsContainer.innerHTML += cards.join(' ');
+};
 openCardCreatorBnt.addEventListener('click', event => {
     openCardCreator();
 });
@@ -13,9 +32,9 @@ const openCardCreator = () => {
     popUpContainer.classList.remove('display__none');
 };
 popUpContainerBntClose.addEventListener('click', event => {
-    closeCardCrator();
+    closeCardCreator();
 });
-const closeCardCrator = () => {
+const closeCardCreator = () => {
     popUpContainer.classList.add('display__none');
 };
 activeCardBnt.addEventListener('click', event => {
@@ -23,13 +42,24 @@ activeCardBnt.addEventListener('click', event => {
 });
 cardsContainer.addEventListener('click', event => {
     const elementClickd = event.target;
-    if (elementClickd.classList.contains('open')) {
-        console.log(elementClickd.id);
+    if (elementClickd.getAttribute('data') == 'interactable') {
+        openPopUpContainerDynamic();
+        makeCardExtendedInDom(seachCardInMyList(parseInt(elementClickd.id)));
     }
     else {
         console.log('fora');
     }
 });
+popUpContainerDynamicCloseBnt.addEventListener('click', event => {
+    popUpContainerDynamicClose();
+});
+const popUpContainerDynamicClose = () => {
+    disableChangeFunctions();
+    popUpContainerDynamic.classList.add('display__none');
+};
+const openPopUpContainerDynamic = () => {
+    popUpContainerDynamic.classList.remove('display__none');
+};
 createNewCardBnt.addEventListener('click', event => {
     const inputChoseImg = cardExtendedCreateNewCard.querySelector('[data="inputChoseImg"]');
     const inputChoseCardColor = cardExtendedCreateNewCard.querySelector('[data="inputChoseCardColor"]');
@@ -41,6 +71,7 @@ createNewCardBnt.addEventListener('click', event => {
     const cardComments = cardExtendedCreateNewCard.querySelector('[data="cardComments"]');
     const checkIfCardIsActivate = activeCardBnt.classList.contains('cardExtended__bntActive_--AC') ? true : false;
     const newCard = {
+        'id': generatesId(),
         'image': inputChoseImg.value,
         'color': inputChoseCardColor.value,
         'name': inputChoseName.value,
@@ -51,6 +82,9 @@ createNewCardBnt.addEventListener('click', event => {
         'comments': cardComments.value,
         'active': checkIfCardIsActivate
     };
+    makeCardInDom(newCard);
+    saveNewCardInMyList(newCard);
+    console.log(MyList);
     inputChoseImg.value = '';
     inputChoseCardColor.value = '';
     inputChoseName.value = '';
@@ -58,20 +92,96 @@ createNewCardBnt.addEventListener('click', event => {
     inputChoseChapter.value = '';
     cardComments.value = '';
     activeCardBnt.classList.remove('cardExtended__bntActive_--AC');
-    makeCardInDom(newCard);
-    console.log(newCard);
-    // alert(inputChoseImg.value)
 });
+const generatesId = () => {
+    let id = 0;
+    if (MyList.length > 0) {
+        id = MyList[MyList.length - 1].id + 1;
+    }
+    return id;
+};
 const makeCardInDom = (card) => {
-    cardsContainer.innerHTML += `
-        <div class="card">
+    return `
+        <div class="card" style="background-color: ${card.color};">
             <div>
                 <img src="${card.image}" alt="">
                 <h3>${card.name}</h3>
             </div> 
-            <div class="card__capa open" id="1">
-                <h3 class="open" id="1">Abrir</h3>
+            <div class="card__capaClickable" data="interactable" id="${card.id}"></div>
+            <div class="card__capa">
+                <h3 class="open">Abrir</h3>
             </div>
         </div>
     `;
 };
+const seachCardInMyList = (id) => {
+    return MyList.find(item => item.id == id);
+};
+const makeCardExtendedInDom = (card) => {
+    popUpContainerDynamicContainer.innerHTML = `
+        <div class="cardExtended__container" style="background-color: ${card.color};" data="cardExtended__container">
+            <div class="cardExtended__img">
+                <img src="${card.image}" alt="" data="interactable" id="img">
+            </div>
+            <div class="cardExtended__info">
+                <input type="color" class="cardExtended__changeColor" data="interactable">
+                <button class="cardExtended__bntDelete cardExtended__centralize">DELETAR</button>
+                <span class="cardExtended__title cardExtended__centralize">Nome da obra <div data="interactable">${card.name}</div></span>
+                <div class="cardExtended__bntActive cardExtended__centralize ${card.active == true ? 'cardExtended__bntActive_--AC' : 0}" data="interactable">
+                    Obra ativa
+                    <button><div></div></button>
+                </div>
+                <span class="cardExtended__season cardExtended__centralize">Temporada/Volume <div data="interactable">${card.season}</div></span>
+                <span class="cardExtended__chapter cardExtended__centralize">Ultimo capitulo visto : <div data="interactable">${card.chapeter}</div></span>
+                <span class="cardExtended__assessment cardExtended__centralize">Sua nota: <div data="interactable">${card.assessment}</div></span>
+                <span class="cardExtended__tag cardExtended__centralize">Gênero: <div data="interactable">${card.tag}</div></span>
+                <span class="cardExtended__comments">
+                    <h3>Comentarios</h3>
+                    <textarea name="" id="" cols="30" rows="10" data="interactable">${card.comments}</textarea>
+                </span>
+            </div>
+        </div>
+    `;
+    activeChangeFunctions();
+};
+const activeChangeFunctions = () => {
+    CurrentExtendedCard = popUpContainerDynamicContainer.querySelector('[data="cardExtended__container"]');
+    CurrentExtendedCard.addEventListener('click', event => {
+        ChangeFunctions(event);
+    });
+};
+const disableChangeFunctions = () => {
+    CurrentExtendedCard.removeEventListener('click', event => {
+        ChangeFunctions(event);
+    });
+};
+const ChangeFunctions = (event) => {
+    const elementClickd = event.target;
+    if (elementClickd.getAttribute('data') == 'interactable') {
+        console.log(elementClickd.id);
+        switch (elementClickd.id) {
+            case 'img':
+                qualuqer(elementClickd);
+                break;
+        }
+    }
+};
+const qualuqer = (elementClickd) => {
+    const el = elementClickd.parentNode;
+    el.innerHTML = `
+        <div class="cardExtended__choseImg cardExtended__input">
+            <label>Insira o link da imagem</label>
+            <input type="text" maxlength="300" data="inputChoseImg">
+        </div>
+    `;
+    console.log('entrou');
+};
+const saveInBrowser = (MyList) => {
+    localStorage.setItem('MinhaListaDeObras', JSON.stringify(MyList));
+};
+const saveNewCardInMyList = (newCard) => {
+    MyList.push(newCard);
+    saveInBrowser(MyList);
+};
+loadMyList();
+loadMyListInDom();
--- MinhaGaleriaDeObras/src/ts/main.ts
+++ MinhaGaleriaDeObras/src/ts/main.ts
@@ -1,3 +1,16 @@
+type card = {
+    'id': number,
+    'image': string,
+    'color': string,
+    'name': string,
+    'season': string,
+    'chapeter': string,
+    'assessment': string,
+    'tag': string,
+    'comments': string,
+    'active': boolean
+}
+
 const openCardCreatorBnt = document.querySelector('[data="openCreateNewCardBnt"]') as HTMLButtonElement
 const popUpContainer = document.querySelector('[data="popUpContainer"]') as HTMLDivElement
 const popUpContainerBntClose = document.querySelector('[data="popUpContainer__bntClose"]') as HTMLButtonElement
@@ -5,6 +18,27 @@ const createNewCardBnt = document.querySelector('[data="createNewCardBnt"]') as 
 const cardExtendedCreateNewCard = document.querySelector('[data="cardExtended__CreateNewCard"]') as HTMLDivElement
 const activeCardBnt = document.querySelector('[data="activeCardBnt"]') as HTMLButtonElement
 const cardsContainer = document.querySelector('[data="cards__container"]') as HTMLDivElement
+const popUpContainerDynamic = document.querySelector('[data="popUpContainerDynamic"]') as HTMLDivElement
+const popUpContainerDynamicCloseBnt = document.querySelector('[data="popUpContainerDynamic__bntClose"]') as HTMLButtonElement
+const popUpContainerDynamicContainer = document.querySelector('[data="popUpContainerDynamicConatiner"]') as HTMLDivElement
+
+let MyList: card[] = []
+let CurrentExtendedCard: HTMLDivElement
+
+const loadMyList = () => {
+    const list: string | null = localStorage.getItem('MinhaListaDeObras')
+    if(list == null) return
+    MyList = JSON.parse(list)
+    console.log(MyList)
+}
+
+const loadMyListInDom = () => {
+    const cards: string[] = []
+    MyList.forEach(card => {
+        cards.push(makeCardInDom(card))
+    })
+    cardsContainer.innerHTML += cards.join(' ')
+}
 
 openCardCreatorBnt.addEventListener('click', event => {
     openCardCreator()
@@ -15,10 +49,10 @@ const openCardCreator = ():void => {
 }
 
 popUpContainerBntClose.addEventListener('click', event => {
-    closeCardCrator()
+    closeCardCreator()
 })
 
-const closeCardCrator = ():void => {
+const closeCardCreator = ():void => {
     popUpContainer.classList.add('display__none')
 }
 
@@ -29,12 +63,26 @@ activeCardBnt.addEventListener('click', event => {
 cardsContainer.addEventListener('click', event => {
     const elementClickd = event.target as HTMLDivElement
     
-    if(elementClickd.classList.contains('open')) {
-        console.log(elementClickd.id)
+    if(elementClickd.getAttribute('data') == 'interactable') {
+        openPopUpContainerDynamic()
+        makeCardExtendedInDom(seachCardInMyList(parseInt(elementClickd.id)))
     } else {
         console.log('fora')
     }
 })
+
+popUpContainerDynamicCloseBnt.addEventListener('click', event => {
+    popUpContainerDynamicClose()
+})
+
+const popUpContainerDynamicClose = ():void => {
+    disableChangeFunctions()
+    popUpContainerDynamic.classList.add('display__none')
+}
+
+const openPopUpContainerDynamic = ():void => {
+    popUpContainerDynamic.classList.remove('display__none')
+}
 
 createNewCardBnt.addEventListener('click', event => {
     const inputChoseImg = cardExtendedCreateNewCard.querySelector('[data="inputChoseImg"]') as HTMLInputElement
@@ -47,7 +95,8 @@ createNewCardBnt.addEventListener('click', event => {
     const cardComments = cardExtendedCreateNewCard.querySelector('[data="cardComments"]') as HTMLTextAreaElement
     const checkIfCardIsActivate = activeCardBnt.classList.contains('cardExtended__bntActive_--AC') ? true : false
 
-    const newCard = {
+    const newCard: card = {
+        'id': generatesId(),
         'image': inputChoseImg.value,
         'color': inputChoseCardColor.value,
         'name': inputChoseName.value,
@@ -59,6 +108,10 @@ createNewCardBnt.addEventListener('click', event => {
         'active': checkIfCardIsActivate
     }
 
+    makeCardInDom(newCard)
+    saveNewCardInMyList(newCard)
+    console.log(MyList)
+    
     inputChoseImg.value = ''
     inputChoseCardColor.value = ''
     inputChoseName.value = ''
@@ -67,21 +120,108 @@ createNewCardBnt.addEventListener('click', event => {
     cardComments.value = ''
     activeCardBnt.classList.remove('cardExtended__bntActive_--AC')
     
-    makeCardInDom(newCard)
-    console.log(newCard)
-    // alert(inputChoseImg.value)
 })
 
-const makeCardInDom = (card:any):void => {
-    cardsContainer.innerHTML += `
-        <div class="card">
+const generatesId = ():number => {
+    let id: number = 0
+    if(MyList.length > 0) {
+        id = MyList[MyList.length -1].id + 1
+    }
+    return id
+}
+
+const makeCardInDom = (card:card):string => {
+    return `
+        <div class="card" style="background-color: ${card.color};">
             <div>
                 <img src="${card.image}" alt="">
                 <h3>${card.name}</h3>
             </div> 
-            <div class="card__capa open" id="1">
-                <h3 class="open" id="1">Abrir</h3>
+            <div class="card__capaClickable" data="interactable" id="${card.id}"></div>
+            <div class="card__capa">
+                <h3 class="open">Abrir</h3>
             </div>
         </div>
     `
 }
+
+const seachCardInMyList = (id: number):card => {
+    return MyList.find(item => item.id == id)!
+}
+
+const makeCardExtendedInDom = (card: card):void => {
+    popUpContainerDynamicContainer.innerHTML = `
+        <div class="cardExtended__container" style="background-color: ${card.color};" data="cardExtended__container">
+            <div class="cardExtended__img">
+                <img src="${card.image}" alt="" data="interactable" id="img">
+            </div>
+            <div class="cardExtended__info">
+                <input type="color" class="cardExtended__changeColor" data="interactable">
+                <button class="cardExtended__bntDelete cardExtended__centralize">DELETAR</button>
+                <span class="cardExtended__title cardExtended__centralize">Nome da obra <div data="interactable">${card.name}</div></span>
+                <div class="cardExtended__bntActive cardExtended__centralize ${card.active == true ? 'cardExtended__bntActive_--AC' : 0}" data="interactable">
+                    Obra ativa
+                    <button><div></div></button>
+                </div>
+                <span class="cardExtended__season cardExtended__centralize">Temporada/Volume <div data="interactable">${card.season}</div></span>
+                <span class="cardExtended__chapter cardExtended__centralize">Ultimo capitulo visto : <div data="interactable">${card.chapeter}</div></span>
+                <span class="cardExtended__assessment cardExtended__centralize">Sua nota: <div data="interactable">${card.assessment}</div></span>
+                <span class="cardExtended__tag cardExtended__centralize">Gênero: <div data="interactable">${card.tag}</div></span>
+                <span class="cardExtended__comments">
+                    <h3>Comentarios</h3>
+                    <textarea name="" id="" cols="30" rows="10" data="interactable">${card.comments}</textarea>
+                </span>
+            </div>
+        </div>
+    `
+    activeChangeFunctions()
+}
+
+const activeChangeFunctions = () => {
+    CurrentExtendedCard = popUpContainerDynamicContainer.querySelector('[data="cardExtended__container"]') as HTMLDivElement
+
+    CurrentExtendedCard.addEventListener('click', event => {
+        ChangeFunctions(event)
+    })
+}
+
+const disableChangeFunctions = () => {
+    CurrentExtendedCard.removeEventListener('click', event => {
+        ChangeFunctions(event)
+    })
+}
+
+const ChangeFunctions = (event:Event) => {
+    const elementClickd = event.target as HTMLInputElement
+    if(elementClickd.getAttribute('data') == 'interactable') {
+        console.log(elementClickd.id)
+        switch(elementClickd.id){
+            case 'img': qualuqer(elementClickd)
+            break;
+        }
+    }
+}
+
+const qualuqer = (elementClickd:any) => {
+    const el = elementClickd.parentNode as HTMLDivElement
+    el.innerHTML = `
+        <div class="cardExtended__choseImg cardExtended__input">
+            <label>Insira o link da imagem</label>
+            <input type="text" maxlength="300" data="inputChoseImg">
+        </div>
+    `
+    console.log('entrou')
+}
+
+const saveInBrowser = (MyList: card[]):void => {
+    localStorage.setItem('MinhaListaDeObras', JSON.stringify(MyList));
+}
+
+const saveNewCardInMyList = (newCard: card):void => {
+    MyList.push(newCard)
+    saveInBrowser(MyList)
+}
+
+
+loadMyList()
+loadMyListInDom()
