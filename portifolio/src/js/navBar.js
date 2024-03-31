2824ca3fa11b52b595c3d61b3055d80230779bf8
Flavio05Henrique <117491467+Flavio05Henrique@users.noreply.github.com>
2024-03-31 15:34:17 -0300

    iniciando seção de projetos
    --background criado.
    --cards de projeto iniciado.

--- portifolio/src/js/navBar.js
+++ portifolio/src/js/navBar.js
@@ -2,28 +2,38 @@ const navBar = document.querySelector('[data="navBar"]');
 const navBarPointer = navBar.querySelector('[data="navBar__pointer"]');
 const sections = document.querySelectorAll('[data="section"]');
 let firstInteraction = false;
+const navBarPointerColors = ["var(--cor_1)", "var(--cor_2)"];
 export const navBarActivateFunctions = () => {
-    console.log(sections);
     setTimeout(() => navBar.style.animation = "bgAnimated 5s infinite", 1000);
     navBar.addEventListener('click', event => {
         const elementClicked = event.target;
         if (elementClicked.tagName == "BUTTON") {
-            const elementClickedAsButton = elementClicked;
-            const elementClickedValues = elementClicked.getClientRects()[0];
-            const elementClickedPositionY = elementClickedValues.y;
-            const elementClickedHeight = elementClickedValues.height;
-            const navBarPointerPosition = elementClickedPositionY + (elementClickedHeight / 2);
-            navBarPointer.style.top = navBarPointerPosition + "px";
-            const index = parseInt(elementClickedAsButton.value);
-            sections[index].click();
-            if (!firstInteraction && elementClickedAsButton.value == '1') {
-                firstInteraction = true;
-                resetSecSkillsBg();
-            }
+            setPointerPosition(elementClicked);
+            slideToSection(elementClicked);
         }
     });
 };
 const resetSecSkillsBg = () => {
     const bg = document.querySelector('[data="secSkills__bg"]');
     bg.classList.add('U-reset_transform');
+};
+const setPointerPosition = (elementClicked) => {
+    const elementClickedValues = elementClicked.getClientRects()[0];
+    const elementClickedPositionY = elementClickedValues.y;
+    const elementClickedHeight = elementClickedValues.height;
+    const navBarPointerPosition = elementClickedPositionY + (elementClickedHeight / 2);
+    navBarPointer.style.top = navBarPointerPosition + "px";
+};
+const slideToSection = (elementClicked) => {
+    const elementClickedAsButton = elementClicked;
+    const index = parseInt(elementClickedAsButton.value);
+    sections[index].click();
+    setPointerColor(index);
+    if (!firstInteraction && elementClickedAsButton.value == '1') {
+        firstInteraction = true;
+        resetSecSkillsBg();
+    }
+};
+const setPointerColor = (number) => {
+    navBarPointer.style.borderRight = `15px solid ${navBarPointerColors[number]}`;
 };
--- portifolio/src/ts/navBar.ts
+++ portifolio/src/ts/navBar.ts
@@ -2,31 +2,17 @@ const navBar = document.querySelector('[data="navBar"]') as HTMLDivElement
 const navBarPointer = navBar.querySelector('[data="navBar__pointer"]') as HTMLDivElement
 const sections = document.querySelectorAll('[data="section"]') as NodeListOf<HTMLDivElement>
 let firstInteraction = false
+const navBarPointerColors: string[] = ["var(--cor_1)", "var(--cor_2)"]
 
 export const navBarActivateFunctions = () => {
-    console.log(sections)
     setTimeout(() => navBar.style.animation = "bgAnimated 5s infinite", 1000)
     
     navBar.addEventListener('click', event => {
         const elementClicked = event.target as HTMLElement
 
         if(elementClicked.tagName == "BUTTON") {
-            const elementClickedAsButton = elementClicked as HTMLButtonElement
-            const elementClickedValues = elementClicked.getClientRects()[0]
-            const elementClickedPositionY = elementClickedValues.y
-            const elementClickedHeight = elementClickedValues.height
-            const navBarPointerPosition =  elementClickedPositionY + (elementClickedHeight / 2)
-
-            navBarPointer.style.top = navBarPointerPosition + "px"
-
-            const index: number = parseInt(elementClickedAsButton.value )
-
-            sections[index].click()
-
-            if(!firstInteraction && elementClickedAsButton.value == '1') {
-                firstInteraction = true
-                resetSecSkillsBg()
-            }
+            setPointerPosition(elementClicked)
+            slideToSection(elementClicked)
         }
     })
 }
@@ -35,3 +21,30 @@ const resetSecSkillsBg = ():void => {
     const bg = document.querySelector('[data="secSkills__bg"]') as HTMLDivElement
     bg.classList.add('U-reset_transform')
 }
+
+const setPointerPosition = (elementClicked: HTMLElement): void => {
+    const elementClickedValues = elementClicked.getClientRects()[0]
+    const elementClickedPositionY = elementClickedValues.y
+    const elementClickedHeight = elementClickedValues.height
+    const navBarPointerPosition =  elementClickedPositionY + (elementClickedHeight / 2)
+
+    navBarPointer.style.top = navBarPointerPosition + "px"
+}
+
+const slideToSection = (elementClicked: HTMLElement): void => {    
+    const elementClickedAsButton = elementClicked as HTMLButtonElement
+    const index: number = parseInt(elementClickedAsButton.value )
+
+    sections[index].click()
+    setPointerColor(index)
+
+    if(!firstInteraction && elementClickedAsButton.value == '1') {
+        firstInteraction = true
+        resetSecSkillsBg()
+    }
+}
+
+const setPointerColor = (number: number):void => {
+    navBarPointer.style.borderRight = `15px solid ${navBarPointerColors[number]}`
+}
+
